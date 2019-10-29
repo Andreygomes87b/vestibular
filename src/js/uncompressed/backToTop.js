a3183e4621ee50a2e29c1bf343ac25d9d53e4b50
Andrey Gomes <andreygomes87b@gmail.com>
2019-10-29 10:38:41 -0300

    JS Change-log:
     - Back to Top .js file modded (now with hovering whatsapp button).
     - Updated version of critical .js loaded first-hand in the project (top_js_uglyfied.js).

--- src/js/uncompressed/backToTop.js
+++ src/js/uncompressed/backToTop.js
@@ -1,11 +1,25 @@
- $(document).ready(function(){
+// This one is the responsible for hiding on scroll the text message additional of the whatsapp button
+// $(document).ready(function(){
+//     $(window).scroll(function () {
+//            if ($(this).scrollTop() < 50) {
+//                $('#whats2').fadeIn();                
+//            } else {
+//                $('#whats2').fadeOut();                
+//            }
+//        });
+//     });
+
+$(document).ready(function(){
      $(window).scroll(function () {
             if ($(this).scrollTop() > 50) {
                 $('#back-to-top').fadeIn();
+                $('#whats').fadeIn();
             } else {
                 $('#back-to-top').fadeOut();
+                $('#whats').fadeOut();
             }
         });
+
         // scroll body to 0px on click
         $('#back-to-top').click(function () {
             $('#back-to-top').tooltip('hide');
@@ -15,5 +29,5 @@
             return false;
         });        
         // $('#back-to-top').tooltip('show');
-
+    
 });
